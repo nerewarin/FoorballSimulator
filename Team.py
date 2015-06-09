f95a7816bb5f2abe1508da0a08359053efe18d0b
nerewarin <nerewarin89@mail.ru>
2015-06-09 22:46:26 +0500

    createTeamFromHTML() - creates list of Team object sorted by UEFAposition createExcelTable - writes book table from Teams (copy HTML)

--- Team.py
+++ Team.py
@@ -1,4 +1,4 @@
-__author__ = '130315'
+__author__ = 'NereWARin'
 # -*- coding: utf-8 -*-
 
 # from lxml import etree as ET
@@ -13,15 +13,25 @@ from lxml import html as html
 # import lxml.html as html
 # from pandas import DataFrame
 
+import xlwt
+
 class Team():
     """
     represents team
     """
-    def __init__(self, name, country, rating, ruName):
+    def __init__(self, name, country, rating, ruName, uefaPos):
         self.name = name
         self.country = country
         self.rating = rating
         self.ruName = ruName
+        self.uefaPos = uefaPos
+        self.methods = ["getUefaPos", "getName", "getRuName", "getCountry", "getRating"]
+
+    def getUefaPos(self):
+        """
+        current position in UEFA rating
+        """
+        return self.uefaPos
 
     def getName(self):
         return self.name
@@ -30,6 +40,9 @@ class Team():
         return self.country
 
     def getRating(self):
+        """
+        points in UEFA rating table
+        """
         return self.rating
 
     def getRuName(self):
@@ -38,26 +51,11 @@ class Team():
     def setRating(self, rating):
         self.rating = rating
 
-Spartak = Team("Spartak Moscow", "RUS", 1, "Спартак Москва")
-teams = {}
-# print "ok"
+    def attrib(self, func_index):
+        return getattr(self, self.methods[func_index])
 
-def parseXML(link):
-    xml_tree = ET.parse(link)
-    root = xml_tree.getroot()
-    print type(root)
-    for child in root:
-        print child
-
-        # barter_obt_item_lst = [int(barter_obtain_item.text) for barter_obtain_item in child.iter(interested_tag)]
-        # if request_id in barter_obt_item_lst:
-        #     answer_ways_count += 1
-        #     answer += str(answer_ways_count) + message + str(child.tag) + "\n"
-
-# print dir(ET)
-UEFA_club_ratingRU = "http://ru.uefa.com/memberassociations/uefarankings/club/"
-UEFA_club_ratingEN = "http://www.uefa.com/memberassociations/uefarankings/club/season=2015/index.html"
-
+def testTeam():
+    Spartak = Team("Spartak Moscow", "RUS", 1, "Спартак Москва", 56)
 
 def unicode_to_str(string):
     """
@@ -76,39 +74,63 @@ def unicode_to_str(string):
 
 # print type(site.xpath("//table[@id=\"clubrank\"]/tbody/tr")), site.xpath("//table[@id=\"clubrank\"]/tbody/tr")
 # HTML parse
-UEFAsiteEN = html.parse(UEFA_club_ratingEN)
-UEFAsiteRU = html.parse(UEFA_club_ratingRU)
-rows_xpathEN = UEFAsiteEN.xpath("//table[@id=\"clubrank\"]/tbody/tr")
-rows_xpathRU = UEFAsiteRU.xpath("//table[@id=\"clubrank\"]/tbody/tr")
-print "teams count =", len(rows_xpathEN)
-team_xpath1 = "td[1]/span[3]/a"
-team_xpath2 = "td[1]/span[3]/span[2]"
-country_xpath = "td[2]"
-UEFArating_xpath = "td[8]"
-# print rows_xpathRU[0].xpath(team_xpath1).pop().text_content()
-for i, tr in enumerate(rows_xpathEN):
-    # print i
-    try:
-        teamName = tr.xpath(team_xpath1).pop().text_content()
-    except IndexError: # special for Betis
-        teamName = tr.xpath(team_xpath2).pop().text_content()
-    try:
-        ruName   = rows_xpathRU[i].xpath(team_xpath1).pop().text_content()
-    except IndexError: # special for Betis
-        ruName   = rows_xpathRU[i].xpath(team_xpath2).pop().text_content()
-    country = tr.xpath(country_xpath).pop().text_content()
-    UEFArating   = tr.xpath( UEFArating_xpath ).pop().text_content()
+def createTeamFromHTML():
+    UEFA_club_ratingRU = "http://ru.uefa.com/memberassociations/uefarankings/club/"
+    UEFA_club_ratingEN = "http://www.uefa.com/memberassociations/uefarankings/club/season=2015/index.html"
+    UEFAsiteEN = html.parse(UEFA_club_ratingEN)
+    UEFAsiteRU = html.parse(UEFA_club_ratingRU)
+    rows_xpathEN = UEFAsiteEN.xpath("//table[@id=\"clubrank\"]/tbody/tr")
+    rows_xpathRU = UEFAsiteRU.xpath("//table[@id=\"clubrank\"]/tbody/tr")
+    teams_count = len(rows_xpathEN)
+    UEFApos_xpath = "td[1]/span[1]"
+    team_xpath1 = "td[1]/span[3]/a"
+    team_xpath2 = "td[1]/span[3]/span[2]"
+    country_xpath = "td[2]"
+    UEFArating_xpath = "td[8]"
 
-        # if site = UEFA_club_ratingEN
-        # print str(i + 1) + ". " + unicode_to_str(team) + " " + country + " " + unicode_to_str(score)
-    # print str(i + 1) + ". " + teamName + " " + country + " " + UEFArating
+    # print rows_xpathRU[0].xpath(team_xpath1).pop().text_content()
+    # MAKE DICT OF TEAMS (teamsD)
+    teamsD = {}
+    teamsL = []
+    for i, tr in enumerate(rows_xpathEN):
+        # print i
+        try:
+            teamName = tr.xpath(team_xpath1).pop().text_content()
+        except IndexError: # special for Betis
+            teamName = tr.xpath(team_xpath2).pop().text_content()
+        # fix error from site
+        if teamName == "1. FSV Mainz 05": teamName = "FSV Mainz 05"
+        try:
+            ruName   = rows_xpathRU[i].xpath(team_xpath1).pop().text_content()
+        except IndexError: # special for Betis
+            ruName   = rows_xpathRU[i].xpath(team_xpath2).pop().text_content()
+        country = tr.xpath(country_xpath).pop().text_content()
+        UEFArating   = tr.xpath( UEFArating_xpath ).pop().text_content()
+        UEFAposition = tr.xpath( UEFApos_xpath ).pop().text_content()
 
-    # create teams
-    teams[teamName] = Team(teamName, country, UEFArating, ruName)
+            # if site = UEFA_club_ratingEN
+            # print str(i + 1) + ". " + unicode_to_str(team) + " " + country + " " + unicode_to_str(score)
+        # print str(i + 1) + ". " + teamName + " " + country + " " + UEFArating
 
-sorted_x = sorted(teams.items(), key=operator.itemgetter(1))
-for i, (team, val) in enumerate(sorted_x):
-    print str(i+1) + ".", team + " (" + val.getRuName() + ")" , val.getCountry(), val.getRating()
+        # create teams
+        teamObj = Team(teamName, country, UEFArating, ruName, UEFAposition)
+        teamsD[teamName] = teamObj
+        teamsL.append(teamObj)
+
+    # # MAKE SORTED LIST OF TEAMS (teamsL)
+    # teamsL = sorted(teamsD.items(), key=operator.itemgetter(1))
+    # for i, (team, val) in enumerate(teamsL):
+    #     # print str(i+1) + ".", team + " (" + val.getRuName() + ")" , val.getCountry(), val.getRating()
+    #     pass
+
+    # teamsL = sorted(teamsD.values(), key=operator.getitem())
+    return teamsL
+
+def printParsedTable(teamsL):
+    for i, team in enumerate(teamsL):
+        print str(i+1) + ".", team.getName() + " (" + team.getRuName() + ")" , team.getCountry(), team.getRating()
+        pass
+
 
 # for key, value in teams.iteritems():
 #     print key, value.getCountry(), value.getRating()
@@ -124,11 +146,38 @@ for i, (team, val) in enumerate(sorted_x):
 # a - текст является ссылкой
 
 
-
-
-
-# print td.pop()['class]']#.keys()
 # короче для твоей цели нужен tree.xpath
 # [15:08:57] Артем Александрович Воробьев: xpath
 # [15:09:14] Артем Александрович Воробьев: тебе и в браузере вернут XPath путь
 # [15:09:23] Артем Александрович Воробьев: и lxml нужно его использовать
+
+# как получить xpath:
+# в браузере жмем F12
+# выбираем лупу, жмем на объект который хотим
+# нас редиректят в правое окно, жмем правой кнопкой мыши - Copy xpath. Можно вставлять
+
+
+# CREATE EXCEL TABLE
+def createExcelTable(filename, teamsL):
+    book = xlwt.Workbook(encoding="utf-8")
+
+    sheet1 = book.add_sheet("Sheet 1")
+
+    headers = ["№", "teamName", "ruName", "country", "rating 2014/2015"]
+    # attributes = ["getName", "getCountry", "getRating", "getRuName"]
+
+    for j, header in enumerate(headers):
+        # print 0, j, header
+        sheet1.write(0, j, header)
+    for i, team  in enumerate(teamsL):
+        for j in range(len(headers)):
+            sheet1.write(i+1, j, team.attrib(j)())
+
+    book.save(filename)
+
+if __name__ == "__main__":
+    testTeam()
+    # create teams list
+    teamsL = createTeamFromHTML()
+    printParsedTable(teamsL)
+    createExcelTable("Rating.xls", teamsL)
